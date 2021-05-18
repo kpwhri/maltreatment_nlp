5d1b917cab844128c028e3d75934963726d01399
dcronkite <dcronkite@gmail.com>
2021-05-18 13:56:43 -0700

    Bug fixes: Correctly ordering terms; forgot to split before joining text on whitespace

--- src/maltreatment_nlp/run.py
+++ src/maltreatment_nlp/run.py
@@ -5,8 +5,8 @@ def run(text, **metadata):
     for name, pat in ALL_PATTERNS.items():
         for m in pat.finditer(text):
             yield metadata | {
-                'pre_context': ' '.join(text[max(m.start() - 100, 0): m.start()]).strip(),
-                'post_context': ' '.join(text[m.end(): m.end() + 100]).strip(),
+                'pre_context': ' '.join(text[max(m.start() - 100, 0): m.start()].split()).strip(),
+                'post_context': ' '.join(text[m.end(): m.end() + 100].split()).strip(),
                 'term': m.group(),
                 'pattern': name,
             }
@@ -14,4 +14,4 @@ def run(text, **metadata):
 
 def get_keys(**metadata):
     """Get all keys, e.g., for creating a csv file with csv.dictwriter"""
-    return ['pre_context', 'post_context', 'term', 'pattern'] + list(metadata.keys())
+    return ['pre_context', 'term', 'post_context', 'pattern'] + list(metadata.keys())
